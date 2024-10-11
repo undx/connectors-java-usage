e3e3d0ccf9bf3eedc42b2eb42f3eb84bde6cfc51
undx <egallois@talend.com>
2024-10-11 16:08:54 +0200

    initial commit

---
+++ src/main/java/org/talend/connectors/internal/WithConnector.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2006-2024 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ */
+package org.talend.connectors.internal;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Marks a class as running under Talend Component Kit context
+ * and makes available ComponentsHandler as an injection.
+ */
+@Target(TYPE)
+@Retention(RUNTIME)
+@ExtendWith(ConnectorsHandler.class)
+public @interface WithConnector {
+
+    /**
+     * @return the package containing the component(s).
+     */
+    String value();
+
+    /**
+     * @return plugin's jar path.
+     */
+    String jarLocation();
+
+    /**
+     * You can isolate some packages during the test.
+     * Note that in the context of a test this can have side effects so ensure to know what you
+     * are doing.
+     *
+     * @return the package loaded from their own classloader.
+     */
+    String[] isolatedPackages() default {};
+}
